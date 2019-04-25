904d24468dbcbe986a00df008fea6e4826e4e039
Shadab Mashuk <shadh06@gmail.com>
2019-04-25 16:23:55 +0100

    Changed role_id type from integer to string
    role_id needed to be in string to allow form validation during registration and login submit

--- AuditCloud/src/main/java/com/c360/auditcloud/controller/LoginController.java
+++ AuditCloud/src/main/java/com/c360/auditcloud/controller/LoginController.java
@@ -2,13 +2,13 @@ package com.c360.auditcloud.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.c360.auditcloud.model.Login;
@@ -17,30 +17,76 @@ import com.c360.auditcloud.service.UserService;
 
 /***
  * Login Controller Class to receive input and provide output back to the Users
+ * 
  * @author Priyaa Thavasimani
  * @date 11 Apr 2019
  */
 @Controller
-@SessionAttributes("login")
+//@SessionAttributes("login")
 public class LoginController {
 	@Autowired
 	UserService userService;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView showLogin(HttpServletRequest request,
-			HttpServletResponse response) {
+	public ModelAndView showLogin(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+		/*
+		 * ModelAndView mav = new ModelAndView("login"); mav.addObject("login", new
+		 * Login()); return mav;
+		 */
+
+		/* Check session for authentication */
+		if (null == session.getAttribute("login")) {
+			System.out.println("Session does not Exist");
+			ModelAndView mav = new ModelAndView("login");
+			mav.addObject("login", new Login());
+			return mav;
+
+		} else {
+
+			ModelAndView mav;
+			if (Integer.parseInt(session.getAttribute("roleid").toString()) == 1) {
+				mav = new ModelAndView("welcomeclient");
+			} else {
+				mav = new ModelAndView("welcomeadmin");
+			}
+
+			return mav;
+		}
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public ModelAndView showLogout(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+			SessionStatus sessionStatus) {
+		System.out.println("Checking logout1 "+session.getAttribute("login"));
+
+		sessionStatus.setComplete();
+		// session.invalidate();
+		session.removeAttribute("login");
+		session.removeAttribute("roleid");
+		System.out.println("Checking logout2 "+session.getAttribute("login"));
+
 		ModelAndView mav = new ModelAndView("login");
 		mav.addObject("login", new Login());
 		return mav;
 	}
 
 	@RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
-	public ModelAndView loginProcess(HttpServletRequest request,
-			HttpServletResponse response, @ModelAttribute("login") Login login) {
+	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+			@ModelAttribute("login") Login login, HttpSession session) {
+		session.setAttribute("login", login);
 		ModelAndView mav = null;
 		User user = userService.validateUser(login);
+		String url = request.getRequestURI();
 		if (null != user) {
-			mav = new ModelAndView("welcome");
+			String welcome = null;
+			System.out.println("Role Id " + Integer.parseInt(user.getRole_id()));
+			session.setAttribute("roleid", Integer.parseInt(user.getRole_id()));
+			if (Integer.parseInt(user.getRole_id()) == 1) {
+				welcome = "welcomeclient";
+			} else {
+				welcome = "welcomeadmin";
+			}
+			mav = new ModelAndView(welcome);
 			mav.addObject("firstname", user.getFirstname());
 			mav.addObject("email", user.getEmail());
 		} else {
--- AuditCloud/src/main/java/com/c360/auditcloud/controller/RegistrationController.java
+++ AuditCloud/src/main/java/com/c360/auditcloud/controller/RegistrationController.java
@@ -2,6 +2,7 @@ package com.c360.auditcloud.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,74 +18,91 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.*;
+
 /***
- * Registration Controller Class to receive input and provide output back to the Users
+ * Registration Controller Class to receive input and provide output back to the
+ * Users
+ * 
  * @author Priyaa Thavasimani
  * @date 11 Apr 2019
  */
 @Controller
-public class RegistrationController  { 
+public class RegistrationController {
 	@Autowired
 	public UserService userService;
 	String encryptedpassword;
-    
-    //Generate Encrypted Password
-    private static String get_SHA_512_SecurePassword(String passwordToHash, byte[] salt)
-    {
-        String generatedPassword = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            md.update(salt);
-            byte[] bytes = md.digest(passwordToHash.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            generatedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
-            e.printStackTrace();
-        }
-        return generatedPassword;
-    }
-     
-    //Add salt
-    private static byte[] getSalt() throws NoSuchAlgorithmException
-    {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
-    }
-    
+
+	// Generate Encrypted Password
+	private static String get_SHA_512_SecurePassword(String passwordToHash, byte[] salt) {
+		String generatedPassword = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(salt);
+			byte[] bytes = md.digest(passwordToHash.getBytes());
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+
+	// Add salt
+	private static byte[] getSalt() throws NoSuchAlgorithmException {
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+		byte[] salt = new byte[16];
+		sr.nextBytes(salt);
+		return salt;
+	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public ModelAndView showRegister(HttpServletRequest request,
-			HttpServletResponse response) {
-		ModelAndView mav = new ModelAndView("register");
-		mav.addObject("user", new User());
-		return mav;
+	public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+		/* Check session for authentication */
+		System.out.println("Checking session "+session.getAttribute("login"));
+		if (null == session.getAttribute("login")) {
+			System.out.println(session.getAttribute("login"));
+			ModelAndView mav = new ModelAndView("register");
+			mav.addObject("user", new User());
+			System.out.println("1");
+			return mav;
+		} else {
+			System.out.println("2");
+			ModelAndView mav;
+			if (Integer.parseInt(session.getAttribute("roleid").toString()) == 1) {
+				mav = new ModelAndView("welcomeclient");
+			} else {
+				mav = new ModelAndView("welcomeadmin");
+			}
+			return mav;
+		}
 	}
 
 	@RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
-	public ModelAndView addUser(HttpServletRequest request,
-			HttpServletResponse response, @ModelAttribute("user") User user) throws NoSuchAlgorithmException {
+	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
+			@ModelAttribute("user") User user) throws NoSuchAlgorithmException {
 		
-		/*Password Encryption*/
-		byte[] salt = getSalt(); //get salt for added security
-		encryptedpassword = get_SHA_512_SecurePassword(user.getPassword(), salt); //add salt to encryption
-		//convert salt from byte array to string to store in database
+		ModelAndView mav = null;
+		byte[] salt = getSalt(); // get salt for added security
+		encryptedpassword = get_SHA_512_SecurePassword(user.getPassword(), salt); // add salt to encryption
 		String convertedSalt = Base64.getEncoder().encodeToString(salt);
 		user.setReset_hash(convertedSalt);
 		user.setPassword_hash(encryptedpassword);
-		/*Ends*/
 		Date created_on = new Date();
 		user.setCreated_on(created_on.toString());
-		System.out.println(user.getPassword_hash());
-		System.out.println(user.getReset_hash());
 		userService.register(user);
-		return new ModelAndView("welcome", "firstname", user.getFirstname());
+		System.out.println("Registration controller " +user.getRole_id());
+		String welcome = null;
+		if (Integer.parseInt(user.getRole_id()) == 1) {
+			welcome  = "welcomeclient";
+		} else {
+			welcome = "welcomeadmin";
+		}
+		mav = new ModelAndView(welcome);
+		mav.addObject("firstname", user.getFirstname());
+		mav.addObject("email", user.getEmail());
+		return mav;
 	}
 }
